8555626ee5732fb0a7e227c3de8a5b9011a66876
nyanky0 <hwngkyo@gmail.com>
2024-03-27 15:27:01 +0700

    helper

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+//component helper
 export const Menunggu = () =>{
     return <div>
         Menunggu ...
     </div>
 }
 
+//functional helper
 export const toLower = (string) =>{
     return string.toLowerCase();
 }
