4386758ada2f75dcf7b94d6484f7f190620868c6
nyanky0 <hwngkyo@gmail.com>
2024-03-27 14:45:53 +0700

    Component lifecycle, helper

--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
 import React, { Component } from 'react';
 import Deskripsi from './component/dumb/Deskripsi';
+import * as Helper from './helper';
+import Loading from './helper/Loading';
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
       name : '',
+      stillType: '',
     }
-    
+    //kalau gapakai arrow function this.setStillType = this.setStillType.bind(this)
     //this.setName = this.setName.bind(this)
   }
   //e=event
   setName = (e) => {
     let name = e.target.value
     this.setState({
-      name : name
+      name : name,
+      stillType : true,
     })
-    console.log(name);
+    //console.log(name);
   }
   //setAlamat (){}
+
+  //setStillType(){
+  //  this.setState({
+  //    stillType : false
+  //  })
+  //}
+  setStillType = () => {
+    this.setState({
+      stillType : false
+    })
+  }
+  componentDidMount(){
+    this.setState({
+      stillType : false,
+    })
+    console.log('Component ini melakukan set')
+  }
+  componentDidUpdate(){
+    console.log('Component melakukan update')
+  }
   render(){
-  return (
-    <div>
+    if(this.state.stillType===true){
+      return(
+        <div>
       <h1>{this.state.name}</h1>
       <input onChange = {this.setName} />
-      <Deskripsi name={this.state.name}></Deskripsi>
+      <button onClick={this.setStillType}>submit</button>
+      <Loading/>
+
     </div>
-  );
+      )
+    }
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <input onChange = {this.setName} />
+        <Deskripsi 
+        name={Helper.toLower(this.state.name)}>
+        </Deskripsi>
+      </div>
+    );
   }
 }
 
---
+++ src/helper/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Menunggu = () =>{
+    return <div>
+        Menunggu ...
+    </div>
+}
+
+export const toLower = (string) =>{
+    return string.toLowerCase();
+}
