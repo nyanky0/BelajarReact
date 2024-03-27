4ec4a5f5d3d86a914386d00921d1db494d452c78
nyanky0 <hwngkyo@gmail.com>
2024-03-27 10:18:51 +0700

    1

---
+++ src/component/dumb/Deskripsi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//function comp using const or function
+const Deskripsi = () => {
+    return <h1>Hello React</h1>
+  }
+  
+function deskripsi1(){
+    return <p>a</p>
+  }
+
+export default Deskripsi;
